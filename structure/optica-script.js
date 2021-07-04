89f51d1d764a08ee52e554ba1dd4f400f1f1f8e5
cristian-cll <78435266+cristian-cll@users.noreply.github.com>
2021-07-04 19:42:04 +0200

    Fix errors on Mysql structure/script

--- structure/optica-script.js
+++ structure/optica-script.js
@@ -3,15 +3,18 @@ db = connection.getDB("optica");
 db.dropDatabase();
 
 db.createCollection("customers");
-db.createCollection("providers");
 db.createCollection("employees");
+db.createCollection("suppliers");
 db.createCollection("glasses");
 db.createCollection("orders");
 
+
 //-------------- Creating customers
+
 
 db.customers.insertOne(
     {
+    _id: 0,
     name: "Xavi",
     email: "xavi@gmail.com",
     phone: 658956232,
@@ -24,13 +27,14 @@ db.customers.insertOne(
         zip: 08018,
         country: "Spain",
     },
-    recommended_by: 2,
-    createdAt: Date.now()
+    recommended_by: null,
+    created_at: Date.now()
     }
 );
 
 db.customers.insertOne(
     {
+    _id: 1,
     name: "Laura",
     email: "laura@gmail.com",
     phone: 658984532,
@@ -43,71 +47,38 @@ db.customers.insertOne(
         zip: 28695,
         country: "Spain",
     },
-    recommended_by: 3,
-    createdAt: Date.now()
+    recommended_by: 0,
+    created_at: Date.now()
     }
 );
+
 
 //-------------- Creating employees
 
+
 db.employees.insertOne(
     {
+    _id: 0,
     name: "Jesús",
-    createdAt: Date.now()
+    created_at: Date.now()
     }
 );
 
 db.employees.insertOne(
     {
+    _id: 1,
     name: "María",
-    createdAt: Date.now()
+    created_at: Date.now()
     }
 );
 
-//-------------- Creating glasses
-
-db.glasses.insertOne(
-    {
-    brand: "RayBan",
-    frame: "Acero",
-    frame_color: "negro",
-    glass_right: {
-        graduation: 0.50,
-        color: "gris"
-    },
-    glass_left: {
-        graduation: 0.50,
-        color: "gris"
-    }, 
-    price: 120,
-    supplier_id: 2,
-    createdAt: Date.now()
-    }
-);
-
-db.glasses.insertOne(
-    {
-    brand: "Guess",
-    frame: "Aluminio",
-    frame_color: "marrón",
-    glass_right: {
-        graduation: 1.50,
-        color: "verde"
-    },
-    glass_left: {
-        graduation: 1.25,
-        color: "verde"
-    }, 
-    price: 180,
-    supplier_id: 1,
-    createdAt: Date.now()
-    }
-);
 
 //-------------- Creating suppliers
 
+
 db.suppliers.insertOne(
     {
+    _id: 0,
     name: "Optica Andorrana",
     nif: "89875865X",
     phone: 658984532,
@@ -121,12 +92,13 @@ db.suppliers.insertOne(
         zip: 89797,
         country: "Andorra",
     },
-    createdAt: Date.now()
+    created_at: Date.now()
     }
 );
 
 db.suppliers.insertOne(
     {
+    _id: 1,
     name: "Gafas SL",
     nif: "23456632T",
     phone: 658984532,
@@ -140,24 +112,83 @@ db.suppliers.insertOne(
         zip: 89797,
         country: "Spain",
     },
-    createdAt: Date.now()
+    created_at: Date.now()
     }
 );
 
-//-------------- Creating stores
+
+//-------------- Creating glasses
+
+
+db.glasses.insertOne(
+    {
+    _id: 0,
+    brand: "RayBan",
+    frame: "Acero",
+    frame_color: "negro",
+    price: 120,
+    grad_left: 0.50,
+    color_left: "gris",
+    grad_right: 0.50,
+    color_right: "gris",
+    supplier_id: 0,
+    created_at: Date.now()
+    }
+);
+
+db.glasses.insertOne(
+    {
+    _id: 1,
+    brand: "Guess",
+    frame: "Aluminio",
+    frame_color: "marrón",
+    price: 180,
+    grad_left: 1.50,
+    color_left: "verde",
+    grad_right: 1.25,
+    color_right: "verde",
+    supplier_id: 1,
+    created_at: Date.now()
+    }
+);
+
+
+//-------------- Creating orders
+
 
 db.orders.insertOne(
     {
-    glasses_id: 1,
-    customer_id: 2,
-    employee_id: 1
+    _id: 0,
+    customer_id: 1,
+    employee_id: 0,
+    glasses: [
+    {
+        glasses_id: 0,
+        price: 120,
+        quantity: 2, 
+    },
+    {
+        glasses_id: 1,
+        price: 180,
+        quantity: 1, 
+    }
+    ],
+    total_price: 360
     }
 );
 
 db.orders.insertOne(
     {
-    glasses_id: 2,
-    customer_id: 2,
-    employee_id: 2
+    _id: 1,
+    customer_id: 0,
+    employee_id: 1,
+    glasses: [
+    {
+        glasses_id: 1,
+        price: 180,
+        quantity: 1, 
+    }
+    ],
+    total_price: 180
     }
 );
--- structure/pizzeria-script.js
+++ structure/pizzeria-script.js
@@ -2,17 +2,75 @@ connection = new Mongo("localhost");
 db = connection.getDB("pizzeria");
 db.dropDatabase();
 
-db.createCollection("customers");
-db.createCollection("stores");
+
 db.createCollection("provinces");
+db.createCollection("stores");
+db.createCollection("customers");
 db.createCollection("employees");
 db.createCollection("products");
 db.createCollection("orders");
 
+
+//-------------- Creating provinces
+
+
+db.provinces.insertOne(
+    {
+    _id: 0,
+    province: "Barcelona",
+    municipes: ["Polinyà", "Cervelló", "Els Prats de Rei", "Torelló", "Barcelona"] 
+    }
+);
+    
+db.provinces.insertOne(
+    {
+    _id: 1,
+    province: "Girona",
+    municipes: ["Aiguaviva", "Alfar", "Bagur", "Besalú", "Blanes"]
+    }
+);
+
+
+//-------------- Creating stores
+
+
+db.stores.insertOne(
+    {
+    _id: 0,
+    address: { 
+        street: "Sepúlveda",
+        number: 58,
+        floor: 2,
+        door: 1,
+        zip: 08018,
+        province: 1,
+        municipe: "Blanes"
+        }
+    }
+);
+
+db.stores.insertOne(
+    {
+    _id: 1,
+    address: { 
+        street: "Av.Catalunya",
+        number: 158,
+        floor: 0,
+        door: 1,
+        zip: 08018,
+        province: 0,
+        municipe: "Barcelona"
+        }
+    }
+);
+
+
 //-------------- Creating customers
+
 
 db.customers.insertOne(
     {
+    _id: 0,
     name: "Xavi",
     surname: "García" ,
     lastname: "Gómez",
@@ -23,16 +81,15 @@ db.customers.insertOne(
         floor: 2,
         door: 1,
         zip: 08018,
-        municipe: 2,
-        province: 08
-
-    },
-    createdAt: Date.now()
+        province: 0,
+        municipe: "Barcelona"
+        }
     }
 );
 
 db.customers.insertOne(
     {
+    _id: 1,
     name: "Laura",
     surname: "García" ,
     lastname: "Gómez",
@@ -44,158 +101,160 @@ db.customers.insertOne(
         floor: 4,
         door: 3,
         zip: 28695,
-        municipe: 2,
-        province: 07
-    },
-
-    createdAt: Date.now()
+        province: 1,
+        municipe: "Blanes"
+        }
     }
 );
+
 
 //-------------- Creating employees
 
-db.employees.insertOne(
-    {
-        name: "Jesús",
-        surname: "Calleja",
-        lastname: "Río",
-        nif: "45256982D",
-        phone: 658232584,
-        role:"repartidor",
-        store_id: 1,
-        createdAt: Date.now()
-    }
-);
 
 db.employees.insertOne(
     {
-        name: "María",
-        surname: "Fernández",
-        lastname: "Aguado",
-        nif: "45256982D",
-        phone: 658232584,
-        role:"cocinera",
-        store_id: 2,
-        createdAt: Date.now()
+    _id: 0,
+    name: "Jesús",
+    surname: "Calleja",
+    lastname: "Río",
+    nif: "45256982D",
+    phone: 658232584,
+    role: "Repartidor",
+    store_id: 0,
     }
 );
 
-//-------------- Creating provinces
-
-db.provinces.insertOne(
-{
-    province: 08,
-    municipe: 1
-}
-);
-
-db.provinces.insertOne(
+db.employees.insertOne(
     {
-        province: 04,
-        municipe: 2
+    _id: 1,
+    name: "María",
+    surname: "Fernández",
+    lastname: "Aguado",
+    nif: "45256982D",
+    phone: 658232584,
+    role: "Cocinero",
+    store_id: 1,
     }
-    );
-
+);
 
 
 //-------------- Creating products
 
-db.products.insertOne(
-    {
-    name: "Pizza",
-    description: "Pepperonni",
-    image: "djidjidj.jpg",
-    price: 8.90,
-    category: "Pizzas",
-    createdAt: Date.now()
-    }
-);
 
 db.products.insertOne(
     {
-    name: "Hamburguesa",
-    description: "Vegana",
-    image: "djidjiddj.jpg",
-    price: 8.90,
-    category: "Hamburguesas",
-    createdAt: Date.now()
-    }
-);
-
-//-------------- Creating stores
-
-db.stores.insertOne(
-    {
-    address : { 
-        street: "Av. Meritxell",
-        number: 5,
-        floor: 1,
-        door: 1,
-        city: "Andorra",
-        zip: 89797,
-        country: "Andorra",
+    _id: 0,
+    name: "BigMac",
+    description: "Hamburguesa idéntica a la del MacDonalds",
+    image: "http://img.com/aisjwi.jpg",
+    type: "Hamburguesa",
+    price: 8.70,
     },
-    province: 08,
-    createdAt: Date.now()
+);
+
+db.products.insertOne(
+    {
+    _id: 1,
+    name: "Coca-Cola",
+    description: "Bebida refrescante",
+    image: "http://img.com/aisjwi.jpg",
+    type: "Bebida",
+    price: 1.50
     }
 );
 
-db.stores.insertOne(
+db.products.insertOne(
     {
-    address : { 
-        street: "Av. Catalunya",
-        number: 5,
-        floor: 1,
-        door: 1,
-        city: "Barcelona",
-        zip: 89797,
-        country: "España",
-    },
-    province: 08,
-    createdAt: Date.now()
+    _id: 2,
+    name: "Pizza Popeye",
+    description: "Pizza vegana",
+    image: "http://img.com/aisjwi.jpg",
+    type: "Pizza",
+    pizza_category: "Vegana",
+    price: 8.20
     }
 );
+
+db.products.insertOne(
+    {
+    _id: 3,
+    name: "Pizza Barbacoa",
+    description: "Pizza con auténtico sabor a barbacoa",
+    image: "http://img.com/aisjwi.jpg",
+    type: "Pizza",
+    pizza_category: "Ahumada",
+    price: 9.70
+    }
+);
+
+db.products.insertOne(
+    {
+    _id: 4,
+    name: "Pizza campesina",
+    description: "Pizza tradicional",
+    image: "http://img.com/aisjwi.jpg",
+    type: "Pizza",
+    pizza_category: "Tradicional",
+    price: 6.90
+    }
+);
+
 
 //-------------- Creating orders
 
+
 db.orders.insertOne(
     {
-    date_time: "11/11/1111 11:11:11",
-    order_type: "domicilio",
-    total_price: 10,
-    store_id: 2,
-    items: [
-        {
-            product_id: 1,
-            quantity: 2
-        },
-        {
-            product_id: 2,
-            quantity: 1
-        }
+    _id: 0,
+    Customer_id: 0,
+    Employee_id: 1,
+    date_time: Date.now(),
+    order_type: "Tienda",
+    item_line: [
+    {
+        Product_id: 0,
+        price: 8.70,
+        quantity: 3
+    },
+    {
+        Product_id: 1,
+        price: 1.50,
+        quantity: 3    
+    },
+    {
+        Product_id: 2,
+        price: 8.20,
+        quantity: 1 
+    }
     ],
-    customer_id: 1,
-    employee_id: 2
+    total_price: 38.8
     }
 );
 
 db.orders.insertOne(
     {
-    date_time: "22/22/2222 11:11:11",
-    order_type: "domicilio",
-    total_price: 15,
-    store_id: 2,
-    items: [
-        {
-            product_id: 1,
-            quantity: 2
-        },
-        {
-            product_id: 2,
-            quantity: 2
-        }
+    _id: 1,
+    Customer_id: 1,
+    Employee_id: 0,
+    date_time: Date.now(),
+    order_type: "Envío",
+    item_line: [
+    {
+        Product_id: 1,
+        price: 1.50,
+        quantity: 2    
+    },
+    {
+        Product_id: 3,
+        price: 9.70,
+        quantity: 1 
+    },
+    {
+        Product_id: 4,
+        price: 6.90,
+        quantity: 1 
+    }
     ],
-    customer_id: 1,
-    employee_id: 2
+    total_price: 19.60
     }
 );
